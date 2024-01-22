8d50bf7660c1ed4882be907853fa147623fd5586
Ekrem <ekrem.oncu18@gmail.com>
2024-01-22 23:30:29 +0300

    lectureYZ.py up

--- lectureYZ.py
+++ lectureYZ.py
@@ -14,6 +14,8 @@ Bu notlar Kaan ASLAN'ın notlarından yararlanılarak oluşturulmuştur.
 Python'a vektörel işlem yapma yeteneğini kazandırmak için bazı üçüncü parti kütüphaneler 
 oluşturulmuştur. Bunların en çok kullanılanı "NumPy" isimli kütüphanedir. Numpy 
 C'de yazılmış bir kütüphanedir.
+
+!!! NumPy dizileri  MUTABLE 'dir. (değiştirilebilir)
 ------------------------------------------------------------------------------------
 
 import numpy  as np
@@ -69,12 +71,255 @@ işaretsiz tamsayı -> Örneğin : (0, 255)
 
 !!! Bir NumPy dizisinin dtype'ı demek -> Onun C dilindeki gerçek(orjinal) türü demek
 ------------------------------------------------------------------------------------
+
+String sınıfı C'deki string sınıfı gibi davranır, iterable değil.
+
+import numpy  as np
+a = np.array('ankara')
+print(a)
+
+a = np.array(list('ankara'))
+print(a)
+------------------------------------------------------------------------------------
+
+İçi sıfırlarla dolu numpy dizilerinin oluşturulması gerekebilmektedir. Bunun için 
+zeros fonksiyonu kullanılmaktadır. zeros fonksiyonun yine birinci parametresi 
+oluşturulacak Numpy dizisinin boyutlarını (shape) belirtir.
+ 
+import numpy  as np
+a = np.zeros(10, dtype='int8') # tek boyutlu dizi
+print(a)
+print()
+
+a = np.zeros( (10,10) , dtype='int8') # iki boyutlu dizi
+print(a)
+------------------------------------------------------------------------------------
+
+ones isimli fonksiyon içi 1'lerle dolu bir NumPy dizisi oluşturmaktadır. Yine fonksiyonun birinci parametresi oluşturulacak dizinin boyutlarını 
+belirtir. dtype parametresi ise dtype türünü belirtir.  Örneğin:
+
+a = np.ones(10, dtype='int32')
+# array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+------------------------------------------------------------------------------------
+
+full isimli fonksiyon NumPy dizisini bizim istediğimiz değerle doldurarak yaratır. 
+(Yani zeros ve ones fonksiyonlarının genel biçimidir.) Bu fonksiyonun yine birinci 
+parametresi NumPy dizisinin boyutlarını, ikinci parametresi doldurulacak değerleri 
+belirtmektedir. Fonksiyonda yine dtype belirtilebilir.
+
+a = np.full(10, 5, dtype='int8')
+# array([5, 5, 5, 5, 5, 5, 5, 5, 5, 5], dtype=int8)
+
+b = np.full((5, 5), 1.2, dtype='float32')
+print(b)
+------------------------------------------------------------------------------------
+"""
+
+
+# ---------- random ------------
+"""
+------------------------------------------------------------------------------------
+
+Rastgele değerlerden NumPy dizisi oluşturabilmek için numpy.random modülünde çeşitli fonksiyonlar bulundurulmuştur. Örneğin 
+numpy.random.random fonksiyonu belli bir boyutta 0 ile 1 arasında rastgele gerçek sayı değerleri oluşturmaktadır. Bu fonksiyon dtype
+parametresine sahip değildir. Her zaman float64 olarak numpy dizisini yaratmaktadır.Fonksiyonun boyut belirten bir parametresi vardır
+------------------------------------------------------------------------------------
+import numpy  as np
+
+a = np.random.random(5)
+#  array([0.16421352, 0.73812732, 0.78583484, 0.37765506, 0.25084917])
+
+a = np.random.random((5, 5))
+# array([[0.90589859, 0.41192256, 0.48235128, 0.57842162, 0.50297967],
+       [0.98173552, 0.80423981, 0.4047245 , 0.85172597, 0.32276178],
+       [0.85301294, 0.26888055, 0.14672257, 0.67893162, 0.06198328],
+       [0.80185088, 0.52359397, 0.35513698, 0.84171051, 0.34803395],
+       [0.15082236, 0.20827871, 0.70286441, 0.46469883, 0.18069756]])
+
+------------------------------------------------------------------------------------
+numpy.random.randint fonksiyonu [low, high) aralığında rastgele tamsayı 
+değerlerinden oluşan NumPy dizisi oluşturmaktadır
+                                 
+a = np.random.randint(10, 20, (5, 5), dtype='int8')
+# array([[15, 14, 12, 13, 14],
+       [16, 13, 13, 10, 12],
+       [12, 15, 15, 15, 19],
+       [18, 13, 13, 14, 13],
+       [14, 12, 18, 19, 14]], dtype=int8)
+------------------------------------------------------------------------------------
 """
 
 
 
+# ---------- arange ------------
+"""
+------------------------------------------------------------------------------------
+
+arange fonksiyonu Python'ın built-in range fonksiyonuna benzemektedir. Ancak arange 
+bize dolaşılabilir bir nesne vermez. Doğrudan bir NumPy dizisi verir. start, stop, 
+step parametreleri range fonksiyonunda olduğu gibidir. Ancak Python range fonksiyonunda 
+start, stop ve step değerleri int türünden olmak zorundayken arange fonksiyonunda 
+float türünden de oabilir. Böylelikle biz arange ile noktasal artırımlarla bir 
+numpy dizisi oluşturabiliriz. arange fonksiyonu dtype parametresi de alabilmektedir. 
+
+Ancak bu fonksiyon her zaman tek boyutlu bir diziyi bize verir.
+------------------------------------------------------------------------------------
+
+import numpy  as np
+a = np.arange(-1, 1, 1)
+# array([-1,  0])
+
+a = np.arange(-1, 1, 0.25)
+#  array([-1.  , -0.75, -0.5 , -0.25,  0.  ,  0.25,  0.5 ,  0.75])
+------------------------------------------------------------------------------------
+
+arange fonksiyonunu kullanırken dikkat etmek gerekir. Çünkü noktasal artırımlar, 
+noktasal start ve stop değerleri yuvarlama hatalarından dolayı beklenenden fazla 
+ya da az sayıda eleman üretebilir. (Örneğin 0.1 artırımlarla ilerleken yuvarlama 
+hatasından dolayı stop değerine çok yakın ama ondan küçük değer elde edilebilir 
+ve bu değer de dizi içinde bulunabilir.) Zaten Python'daki built-in range sınıfının 
+tamsayı değerler almasının nedeni de budur. 
+------------------------------------------------------------------------------------
+"""
 
 
+# ---------- linspace ------------
+"""
+------------------------------------------------------------------------------------
+arange fonksiyonun yukarıda belirtilen probleminden dolayı noktasal artırım için 
+genellikle programcılar linspace fonksiyonunu tercih ederler. Bu fonksiyon start, 
+stop ve num parametrelerine sahiptir. Fonksiyon her zaman start ve stop değerlerini 
+de içerecek biçimde eşit aralıklı num tane değeri oluşturarak onu bir NumPy dizisi 
+olarak vermektedir. linspace ile elde edilecek eleman sayısı belli olduğu için 
+arange fonksiyonu yerine genellikle programcılar bunu tercih etmektedir. linspace 
+fonksiyonun dtype parametresi de vardır. Bu parametre için argüman girilmezse 
+default detype np.float64 olacak biçimde belirlenir. 
+
+import numpy  as np
+
+a = np.linspace(0, 10, 10)
+# array([ 0.        ,  1.11111111,  2.22222222,  3.33333333,  4.44444444,
+        5.55555556,  6.66666667,  7.77777778,  8.88888889, 10.        ])
+------------------------------------------------------------------------------------
+"""
 
 
+# ---------- shape ------------
+"""
+------------------------------------------------------------------------------------
+Bir NumPy dizisinin boyutlarını (yani kaça kaçlık olduğunu) shape isimli özniteliği 
+ile elde edebiliriz. shape özniteliği bize boyutları belirten bir demet vermektedir.
+Örneğin:
+
+a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+print(a.shape) # (3,4)
+------------------------------------------------------------------------------------
+"""
+
+# ---------- reshape ------------
+"""
+------------------------------------------------------------------------------------
+Bir NumPy dizisinin boyutları değiştirilebilir. Bunun için ndarray sınıfının 
+reshape metodu ya da reshape fonksiyonu kullanılabilemktedir. Default durumda 
+ndarray elemanları C Programlama Dilindeki gibi satırsal biçimde belleğe tek 
+boyutlu olarak yerleştirilmektedir.
+
+Örneğin aşağıdaki gibi bir NumPy dizisi bulunuyor olsun:
+
+1 2 3 4 
+5 6 7 8 
+
+Bu aslında bu dizi bellekte şu sırada tek boyutlu biçimde tutulmaktadır:
+
+1 2 3 4 5 6 7 8 
+
+Şimdi biz bu diziyi 4x2 olarak reshape yaparsak dizi şu hale gelir:
+
+1 2
+3 4
+5 6
+7 8
+
+Yani reshape işlemini şöyle düşünmelisiniz: Sanki önce çok boyutlu dizi tek boyuta 
+dönüştürülüp, yeniden diğer boyuta dönüştürülmektedir. 
+
+reshape işleminden yeni bir NumPy dizisi elde edilmektedir.
+------------------------------------------------------------------------------------
+
+import numpy  as np
+a = np.random.randint(0, 100, (5, 4))
+print(a)
+print()
+
+b = a.reshape(2,10)
+print(b)
+------------------------------------------------------------------------------------
+
+Çok boyutlu dizilerin tek boyutlu hale getirilmesi çokça gereksinim duyulan bir 
+işlemdir. Örneğin elimizde 5x4'lük bir NumPy dizisi olsun. Biz bunu tek boyutlu 
+bir dizi haline getirmeye çalışalım. Tabii bu işlemi reshap metoduyla ya da 
+fonksiyonuyla yapabiliriz. Örneğin;
+
+a = np.random.randint(0, 100, (5, 4))
+b = a.reshape(20)
+
+Ancak bu işlem için ravel isimli bir metot ve global bir fonksiyon da 
+bulundurulmuştur. ravel bize reshape işleminde olduğu gibi bir view nesnesi 
+vermektedir. Örneğin:
+
+c = a.ravel()
+
+------------------------------------------------------------------------------------
+"""
+
+
+# ---------- slicing (dilimleme)------------
+"""
+------------------------------------------------------------------------------------
+NumPy dizileri üzerinde dilimleme (slicing) yapılabilir. Dilimleme işleminde tamamen
+Python listelerindeki semantik uygulanmaktadır. Dilimleme her boyut için ayrı ayrı 
+yapılabilmektedir. Dilimleme işleminden yeni bir NumPy dizisi view nesnesi olarak 
+elde edilmektedir. Dilimleme ilk boyuttan başlanarak boyut gerçekleştirilmektedir.
+
+import numpy as np
+
+a = np.random.randint(102,357, (7,7), dtype= 'int32')
+print(a)
+print()
+
+print(a[1,2])
+print()
+
+print(a[2])
+print()
+
+print(a[2, 3:7])
+print()
+
+print(a[:, 2:5])
+print()
+
+
+a[:, 2:5] = 0
+print(a)
+print()
+------------------------------------------------------------------------------------
+"""
+
+# bool indeksleme 
+"""
+------------------------------------------------------------------------------------
+Bir NumPy dizisine bool indeksleme uygulanabilir. bool indeksleme için dizi uzunluğu ve boyutu kadar bool türden dolaşılabilir bir nesne
+girilir. Bu dolaşılabilir nesnedeki True olan elemanlara karşı gelen dizi elemanları elde edilmektedir.
+
+import numpy as np
+a = np.array( [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ])
+
+b = [True, False, True, False, True, True, False, True, True]
+
+c = a[b]
+
+print(c)
+------------------------------------------------------------------------------------
+"""
 
