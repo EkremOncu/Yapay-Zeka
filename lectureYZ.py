c75c9494b1ecaad9aefadf4bbb2044c31e8382ae
Ekrem <ekrem.oncu18@gmail.com>
2024-01-22 17:58:03 +0300

    YapayZeka started

---
+++ lectureYZ.py
@@ -0,0 +1,7 @@
+"""
+---------------------------------------------------------------------------
+C ve Sistem Programcıları Derneği Sınıfta Yapılan Örnekler ve Özet Notlar
+                                
+Bu notlar Kaan ASLAN'ın notlarından yararlanılarak oluşturulmuştur. 
+---------------------------------------------------------------------------
+"""
