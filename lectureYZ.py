926fc22e40ddcaf3390b43f3cdf97cdb6c99a594
Ekrem <ekrem.oncu18@gmail.com>
2024-01-22 20:39:09 +0300

    lectureYZ.py update

--- lectureYZ.py
+++ lectureYZ.py
@@ -5,3 +5,76 @@ C ve Sistem Programcıları Derneği Sınıfta Yapılan Örnekler ve Özet Notla
 Bu notlar Kaan ASLAN'ın notlarından yararlanılarak oluşturulmuştur. 
 ---------------------------------------------------------------------------
 """
+
+
+
+#  ----------------------------- NumPy  -----------------------------
+"""
+------------------------------------------------------------------------------------
+Python'a vektörel işlem yapma yeteneğini kazandırmak için bazı üçüncü parti kütüphaneler 
+oluşturulmuştur. Bunların en çok kullanılanı "NumPy" isimli kütüphanedir. Numpy 
+C'de yazılmış bir kütüphanedir.
+------------------------------------------------------------------------------------
+
+import numpy  as np
+
+a = np.array([  [1, 2, 3], [4, 5, 6], [7, 8, 9] ])
+print(a, type(a))
+------------------------------------------------------------------------------------
+
+Anımsanacağı gibi Python'daki list, tuple, gibi veri yapıları aslında değerlerin 
+kendisini tutmamaktadır. Değerlerin tutulduğu nesnelerin adreslerini tutmaktadır. 
+Bu biçimdeki çalışma yoğun sayısal işlemlerde oldukça hantal hale gelmektedir. Bu 
+nedenle NumPy dizileri değerleri Python'un listeleri gibi değil C Programlama 
+Dilindeki diziler gibi tutmaktadır. Yani NumPy dizilerinin elemanları genel olarak 
+aynı türdendir.Örneğin:
+
+a = np.array([1, 2, 3, 4, 5])
+a.dtype
+
+Burada a değişkenin gösteridği NumPy dizisi tamamen C'deki gibi bir dizidir. Yani 
+NumPy dizisinin elemanları adresleri değil doğrudan değerlerin kendisini tutmaktadır. 
+Bu nedenle NumPy dizileri (yani ndarray nesneleri) birkaç istisna durum dışında 
+homojendir.
+------------------------------------------------------------------------------------
+
+array fonksiyonuyla NumPy dizisi (ndarray nesnesi) yaratılırken yaratılacak NumPy 
+dizisinin C'deki dtype türü array fonksiyonun dtype parametresi ile açıkça 
+belirlenebilir.
+
+a = np.array([1, 2, 3, 4, 5], dtype= 'float64' )
+a = np.array([1, 2, 3, 4, 5], dtype= np.float64 )
+#  array([1., 2., 3., 4., 5.])
+
+ NumPy dizilerinde kullanabileceğimiz dtype türlerinin önemli olanları şunlardır: 
+
+bool_ /bool8        : bool türü
+byte / int8         : bir byte'lık işaretli tamsayı türü
+ubyte / uint8       : bir byte'lık işaretsiz tamsayı türü
+short / int16       : iki byte'lık işaretli tamsayı türü
+ushort / uint16     : iki byte'lık işaretsiz tamsayı türü
+int32               : dört byte'lık işaretli tamsayı türü
+uint32              : dört byte'lık işaretsiz tamsayı türü
+int64               : sekiz byte2lık işaretli tamsayı türü
+uint64              : sekiz byte'lık işaretsiz tamsayı türü
+float32 / single    : dört byte'lık gerçek sayı türü
+
+float64 / float / double : sekiz byte'lık gerçek sayı türü
+
+
+işaretli tamsayı -> Örneğin : (-128, 127)
+işaretsiz tamsayı -> Örneğin : (0, 255)
+
+-> işaretli tamsayı = bitlerden biri işarete ayrılmış demek
+
+!!! Bir NumPy dizisinin dtype'ı demek -> Onun C dilindeki gerçek(orjinal) türü demek
+------------------------------------------------------------------------------------
+"""
+
+
+
+
+
+
+
+
