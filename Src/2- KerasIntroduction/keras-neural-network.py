4d350c6df70e865ce2608776b3d000c2ca074ab2
Ekrem <ekrem.oncu18@gmail.com>
2024-04-06 23:10:27 +0300

    keras-neural-network.py up

--- Src/2- KerasIntroduction/keras-neural-network.py
+++ Src/2- KerasIntroduction/keras-neural-network.py
@@ -30,8 +30,24 @@ model.summary()
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['binary_accuracy'])
 model.fit(training_dataset_x, training_dataset_y, batch_size=32, epochs=100, validation_split=0.2)
+eval_result = model.evaluate(test_dataset_x, test_dataset_y, batch_size=32)
 
+for i in range(len(eval_result)):
+    print(f'{model.metrics_names[i]}: {eval_result[i]}')
 
+import numpy as np
+
+predict_dataset = np.array([[2 ,90, 68, 12, 120, 38.2, 0.503, 28],
+                            [4, 111, 79, 47, 207, 37.1, 1.39, 56],
+                            [3, 190, 65, 25, 130, 34, 0.271, 26],
+                            [8, 176, 90, 34, 300, 50.7, 0.467, 58],
+                            [7, 106, 92, 18, 200, 35, 0.300, 48]])
+
+predict_result = model.predict(predict_dataset)
+print(predict_result)
+
+for result in predict_result[:, 0]:
+    print('Şeker hastası' if result > 0.5 else 'Şeker Hastası Değil')
 
 
 
