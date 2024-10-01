e94f48e819896ea1eeb5e32f7dfc3d793440eb58
Ekrem Öncü <128517044+EkremOncu@users.noreply.github.com>
2024-10-01 13:57:57 +0300

    conv1d-test.py

---
+++ Src/26- Conv1D/conv1d-test.py
@@ -0,0 +1,10 @@
+from tensorflow.keras.layers import Conv1D
+import numpy as np
+
+we_text = np.random.random((6, 8))
+
+conv1d = Conv1D(1, 2)
+
+result = conv1d(we_text.reshape((1, 6, 8)))
+print(result.shape)
+
