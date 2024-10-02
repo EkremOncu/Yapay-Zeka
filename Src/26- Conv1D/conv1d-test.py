874541fe7b03dc54c8abf52e8ac65c11128221a7
Ekrem Öncü <128517044+EkremOncu@users.noreply.github.com>
2024-10-02 15:28:32 +0300

    conv1d-test.py adj

--- Src/26- Conv1D/conv1d-test.py
+++ Src/26- Conv1D/conv1d-test.py
@@ -5,6 +5,6 @@ we_text = np.random.random((6, 8))
 
 conv1d = Conv1D(1, 2)
 
-result = conv1d(we_text.reshape((1, 6, 8)))
+result = conv1d(we_text.reshape((1, 6, 8))) # girdi 3 boyutlu olmalı 
 print(result.shape)
 
