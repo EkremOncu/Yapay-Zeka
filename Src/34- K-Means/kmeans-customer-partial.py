80328b8b8eb51225cac28cebc24ebe0e2d9ee611
Ekrem Öncü <128517044+EkremOncu@users.noreply.github.com>
2024-12-24 00:17:58 +0300

    kmeans-customer-partial.py adj

--- Src/34- K-Means/kmeans-customer-partial.py
+++ Src/34- K-Means/kmeans-customer-partial.py
@@ -1,61 +1,50 @@
+NCLUSTERS = 5
 BATCH_SIZE = 10
-
-import pandas as pd
-
-tfr = pd.read_csv('segmentation data.csv', chunksize=BATCH_SIZE)
-
-for df in tfr:
-    df.drop(labels=['ID'], axis=1, inplace=True)
-    ohe_df = pd.get_dummies(df, columns=['Education', 'Occupation', 'Settlement size'], dtype='uint8')
-    print(df)
-    
-
-
-from sklearn.preprocessing import StandardScaler
-
-ss = StandardScaler()
-scaled_dataset = ss.fit_transform(ohe_df.to_numpy())
-
-from sklearn.cluster import KMeans
-
-total_inertias = [KMeans(n_clusters=i, n_init=10).fit(scaled_dataset).inertia_ for i in range(1, 100)]
-
-
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(15, 8))
-plt.title('Elbow Graph')
-plt.plot(range(1, 100), total_inertias, marker='o')
-plt.xticks(range(1, 100, 5))
-plt.show()
 
 
 import numpy as np
-from sklearn.metrics import silhouette_score
-
-optimal_cluster = np.argmax([silhouette_score(scaled_dataset, KMeans(i, n_init=10).fit(scaled_dataset).labels_) 
-                             for i in range(2, 10)]) + 3
-
-km = KMeans(n_clusters=optimal_cluster, n_init=100)
-km.fit(scaled_dataset)
+import pandas as pd
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.preprocessing import OneHotEncoder
 
 
-from sklearn.decomposition import PCA
-
-pca = PCA(n_components=2)
-transformed_dataset = pca.fit_transform(scaled_dataset)
-
-plt.figure(figsize=(12, 10))
-plt.title('Clustered Points')
-for i in range(1, optimal_cluster + 1):
-    plt.scatter(transformed_dataset[km.labels_ == i, 0], transformed_dataset[km.labels_ == i, 1])
-plt.show()
+mbkm = MiniBatchKMeans(n_clusters=NCLUSTERS, batch_size=BATCH_SIZE)
 
 
+numeric_features = ['Age', 'Income']
+categoric_features = ['Education', 'Occupation', 'Settlement size']
+binary_features = ['Sex', 'Marital status']
 
 
+unique_list = [set(), set(), set()]
+count = 0
+total = 0
+total_square = 0
+for df in pd.read_csv('segmentation data.csv', chunksize=BATCH_SIZE):
+    unique_list[0].update(df['Education'])
+    unique_list[1].update(df['Occupation'])
+    unique_list[2].update(df['Settlement size'])
+    
+    total += df[numeric_features].sum()
+    total_square += (df[numeric_features] ** 2).sum()
+    count += df.shape[0]
+    
+numeric_mean = total / count
+numeric_std = (total_square / count - numeric_mean  ** 2) ** 0.5
 
 
+unique_list = [list(us) for us in unique_list]
+ohe = OneHotEncoder(sparse_output=False, categories=unique_list)
 
+
+for df in pd.read_csv('segmentation data.csv', chunksize=BATCH_SIZE):
+    categoric_array = ohe.fit_transform(df[categoric_features])
+    numeric_array = ((df[numeric_features] - numeric_mean) / numeric_std).to_numpy()
+    binary_array = df[binary_features].to_numpy()
+    combined_array = np.hstack((categoric_array, binary_array, numeric_array))
+    mbkm.partial_fit(combined_array)
+
+
+print(mbkm.labels_)        
 
 
