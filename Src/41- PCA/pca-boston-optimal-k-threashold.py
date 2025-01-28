a35e1a56143ec797b768ea2f5d289c828c329a82
Ekrem Öncü <128517044+EkremOncu@users.noreply.github.com>
2025-01-28 13:26:30 +0300

    pca-boston-optimal-k-threashold.py

---
+++ Src/41- PCA/pca-boston-optimal-k-threashold.py
@@ -0,0 +1,26 @@
+TARGET_RATIO = 0.7
+
+import pandas as pd
+
+df = pd.read_csv('housing.csv', delimiter=r'\s+', header=None)
+
+dataset_x = df.iloc[:, :-1].to_numpy(dtype='float32')
+dataset_y = df.iloc[:, -1].to_numpy(dtype='float32')
+
+from sklearn.preprocessing import StandardScaler
+
+ss = StandardScaler()
+ss.fit(dataset_x)
+scaled_dataset_x = ss.transform(dataset_x)
+
+import numpy as np
+from sklearn.decomposition import PCA
+
+for i in range(1, dataset_x.shape[1] + 1):
+    pca = PCA(i)
+    pca.fit(scaled_dataset_x)
+    total_ratio = np.sum(pca.explained_variance_ratio_)
+    if total_ratio >= TARGET_RATIO:
+        break
+    
+print(f'Number of features: {i}')
