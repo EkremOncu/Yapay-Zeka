e425249d8e8ac5229655d914e832ebc849a3ae1d
Ekrem Öncü <128517044+EkremOncu@users.noreply.github.com>
2025-02-24 20:44:25 +0300

    sklearn-linear-regression-boston.py

---
+++ Src/47- LinearRegression/sklearn-linear-regression-boston.py
@@ -0,0 +1,75 @@
+import pandas as pd
+
+df = pd.read_csv('housing.csv', delimiter=r'\s+', header=None)
+
+dataset_x = df.iloc[:, :-1].to_numpy(dtype='float32')
+dataset_y = df.iloc[:, -1].to_numpy(dtype='float32')
+
+from sklearn.model_selection import train_test_split
+
+training_dataset_x, test_dataset_x, training_dataset_y, test_dataset_y = train_test_split(dataset_x, dataset_y, test_size=0.2)
+
+from sklearn.linear_model import LinearRegression
+
+lr = LinearRegression()
+lr.fit(training_dataset_x, training_dataset_y)
+
+predict_result = lr.predict(test_dataset_x)
+
+from sklearn.metrics import mean_absolute_error
+
+mae = mean_absolute_error(predict_result, test_dataset_y)
+print(f'Mean Absolute Error: {mae}')
+
+r2 = lr.score(test_dataset_x, test_dataset_y)
+print(f'R^2 = {r2}')
+print()
+
+from sklearn.preprocessing import StandardScaler
+
+ss = StandardScaler()
+ss.fit(training_dataset_x)
+scaled_training_dataset_x = ss.transform(training_dataset_x)
+scaled_test_dataset_x = ss.transform(test_dataset_x)
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+
+model = Sequential(name='Boston-Housing-Price')
+model.add(Dense(64, activation='relu', input_dim=training_dataset_x.shape[1], name='Hidden-1'))
+model.add(Dense(64, activation='relu', name='Hidden-2'))
+model.add(Dense(1, activation='linear', name='Output'))
+model.summary()
+
+model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+hist = model.fit(scaled_training_dataset_x, training_dataset_y, batch_size=32, epochs=200, validation_split=0.2)
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(15, 5))
+plt.title('Epoch-Loss Graph', fontsize=14, fontweight='bold')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.xticks(range(0, 210, 10))
+
+plt.plot(hist.epoch, hist.history['loss'])
+plt.plot(hist.epoch, hist.history['val_loss'])
+plt.legend(['Loss', 'Validation Loss'])
+plt.show()
+
+plt.figure(figsize=(15, 5))
+plt.title('Epoch-Mean Absolute Error Graph', fontsize=14, fontweight='bold')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.xticks(range(0, 210, 10))
+
+plt.plot(hist.epoch, hist.history['mae'])
+plt.plot(hist.epoch, hist.history['val_mae'])
+plt.legend(['Mean Absolute Error', 'Validation Mean Absolute Error'])
+plt.show()
+
+eval_result = model.evaluate(scaled_test_dataset_x, test_dataset_y)
+
+for i in range(len(eval_result)):
+    print(f'{model.metrics_names[i]}: {eval_result[i]}')
+    
